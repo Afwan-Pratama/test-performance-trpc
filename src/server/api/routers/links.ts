1e0d45b7ab6384ff7622c9edf563715693d02226
Afwan-Pratama <phil.pratama@gmail.com>
2023-01-19 12:16:03 +0700

    add : initial node js

--- src/server/api/routers/links.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-import { createTRPCRouter, publicProcedure } from '../trpc'
-
-export const linksRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.link.findMany()
-  }),
-})
